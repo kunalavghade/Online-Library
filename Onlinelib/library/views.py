79c0afc1707eecf3890b82f3c5a93a0358fd43ea
kunalavghade <kunalavghade77@example.com>
2021-09-21 19:08:25 +0530

    addhed singin/signup

--- Onlinelib/library/views.py
+++ Onlinelib/library/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render,HttpResponse
+from library.models import Student
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        password = request.POST.get('password')
+        date = request.POST.get('date')
+        data = Student(date=date,name=username,email=email,phone=phone,address=address,password=password)
+        data.save()
+        print(username,email,phone,address,password,date)
     return render(request,"signup.html")    
+
 def login(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        try:
+            data=Student.objects.get(name=username,password=password)
+            print(data.name, data.password)
+            return render(request,'index.html')
+        except:
+            return render(request,'login.html')
+    return render(request,'login.html')        
+
+
 def logout(request):
     return HttpResponse("login page")
