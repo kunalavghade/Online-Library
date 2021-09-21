42639d5f557f06e31c96a69774a7df3926fed9bc
shubham-kite <shubhamkites099@gmail.com>
2021-09-21 11:01:19 +0530

    added signup function

--- Onlinelib/library/views.py
+++ Onlinelib/library/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def signup(request):
+    return render(request,"signup.html")    
 def login(request):
-    return HttpResponse("login page")
+    return render(request,'login.html')
 def logout(request):
     return HttpResponse("login page")
